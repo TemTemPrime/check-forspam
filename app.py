a1c5ee81f20e8ed9797def8d5ee476f18f597fa2
TemTemPrime <ephraimplayz@gmail.com>
2026-03-16 11:10:31 +0100

    first?

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import joblib 
+
+@st.cache_resource
+def load_model():
+    model = joblib.load("email_spam model.pkl")  
+    return model
+st.title("email spam prediciton")
+st.write("Insert email below")
+
+email = st.text_input("Insert Email")
+
+
+if st.button("predict"):
+    if email.strip() == "":
+        st.warning("Please enter an email message.")
+    else:
+        prediction = model.predict([email])[0]
+
+        if prediction == 1:
+            st.error("🚨 This email is SPAM")
+        else:
+            st.success("✅ This email is NOT Spam")
